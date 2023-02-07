492b13991f5248481ff9183c0ee2a9fbe4ba95f2
Padraig Osborne <pkeegan97@gmail.com>
2023-02-06 18:22:15 -0800

    make event.preventDefault functional

--- program-lang/js/scripts.js
+++ program-lang/js/scripts.js
@@ -1,40 +1,39 @@
-    form.onload = function() {
-    let form = document.querySelector("form");
-    form.onsubmit = function(event) {
+window.onload = function () {
+    const form = document.querySelector("form");
+    form.onsubmit = function (e) {
+      e.preventDefault();
     };
 };
-function getResults(event) {
+function getResults() {
     let form = document.getElementsByClassName("had")
     const question1 = parseInt(document.querySelector("input[name='question1']:").valueOf);
     const question2 = parseInt(document.querySelector("input[name='question2']:").valueOf);
     const question3 = parseInt(document.querySelector("input[name='question3']:").valueOf);
     const question4 = parseInt(document.querySelector("input[name='question4']:").valueOf);
     const question5 = parseInt(document.querySelector("input[name='question5']:").valueOf);
-    const resultRange = ("question1.valueOf" + "question2.valueOf" + "question3.valueOf" + "question4.valueOf" + "question5.valueOf") = (resultRange);
-}
+    let resultRange = ("art" || "math" + "car" || "bike" + "comic" || "novel" + "alot" || "none" + "hw" || "mb");
+    };
+    
 function hideResults() {
     document.getElementById("ruby").setAttribute("class", "hidden");
     document.getElementById("java").setAttribute("class", "hidden");
     document.getElementById("python").setAttribute("class", "hidden");
 }
-    if (resultRange = "5") {
-    document.getElementById("ruby").removeAttribute("class", "hidden");
+    if (resultRange = 5) {
+    document.getElementById("ruby")
+    Element.removeAttribute("class", "hidden");
 }
-    else if (resultRange <= "8") {
-        document.getElementById("java").removeAttribute("class","hidden");   
+    else if (resultRange <=6) {
+        document.getElementById("java")
+        Element.removeAttribute("class", "hidden");   
 }
-    else (resultRange >= "9") 
-        document.getElementById("python").removeAttribute("class","hidden");
+    else (resultRange <=9) 
+        document.getElementById("python")
+        Element.removeAttribute("class","hidden");
 
 
-    form.addEventListener("submit", resultRange), function() {
-        let form = document.getElementsByClassName("had")
-        reset.removeAttrtibute("class", "hidden");
-    }
-
-    window.addEventListener("submit", function (event) {
+    form.addEventListener("submit", function (e) {
         let form = document.getElementsByClassName("had");
         form.addEventListener("submit", resultRange);
-        form.onsubmit = function() {
-        event.preventDefault();
+        form.onsubmit = function(e) {
     }});
